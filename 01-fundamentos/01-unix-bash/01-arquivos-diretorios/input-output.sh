dcfce607feff3afdb4051135dc80b1d27b244ee8
Marlon Couto <marloncouto.dev@gmail.com>
2022-11-28 18:41:08 -0300

    Atualiza o script input-output.sh
    Signed-off-by: Marlon Couto <marloncouto.dev@gmail.com>

---
+++ 01-fundamentos/01-unix-bash/01-arquivos-diretorios/input-output.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Interage com o diretório unix_tests_skills
+mkdir unix_tests_skills
+cd unix_tests_skills
+
+# Adiciona o arquivo skills2.txt ao diretório
+cat <<EOT >>skills2.txt
+Internet
+Unix
+Bash
+EOT
+
+# Adiciona mais 5 linhas o arquivo skills2.txt
+cat <<EOT >>skills2.txt
+HTML
+CSS
+JS
+Jest
+React
+EOT
