898439d82b2d1886652aa35eda69c81d42b9ffb5
Marlon Couto <marloncouto.dev@gmail.com>
2022-11-28 22:23:10 -0300

    Atualiza input-output.sh
    Signed-off-by: Marlon Couto <marloncouto.dev@gmail.com>

--- 01-fundamentos/01-unix-bash/02-unix-bash/input-output.sh
+++ 01-fundamentos/01-unix-bash/02-unix-bash/input-output.sh
@@ -26,8 +26,18 @@ wc -l skills2.txt
 head -3 skills2.txt | sort >top_skills.txt
 
 # Cria o arquivo phrases2.txt
-cat <<EOT >>phrases2.txt
+cat <<EOT >phrases2.txt
 A vida é a arte do encontro, embora haja tanto desencontro pela vida
 O sucesso geralmente vem para aqueles que estão ocupados demais para procurá-lo
 Penso, logo existo
+EOT
+
+# interage com o arquivo phrases2.txt
+grep br phrases2.txt | wc -l
+grep -v br phrases2.txt | wc -l
+
+# Adiciona dois nomes de países ao final do arquivo
+cat <<EOT >>phrases2.txt
+Brasil
+Japão
 EOT
