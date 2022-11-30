9bff33251405c11b2214702ccb7ad06015164b7d
Marlon Couto <marloncouto.dev@gmail.com>
2022-11-30 16:26:24 -0300

    Atualiza processos.sh
    Signed-off-by: Marlon Couto <marloncouto.dev@gmail.com>

--- 01-fundamentos/01-unix-bash/02-unix-bash/processos.sh
+++ 01-fundamentos/01-unix-bash/02-unix-bash/processos.sh
@@ -7,3 +7,23 @@ ps
 sleep 30 &
 ps | grep sleep
 kill %1
+sleep 30
+bg
+
+# Mais interações com sleep
+sleep 300 &
+sleep 200 # (suspender)
+sleep 100 # (suspender)
+jobs
+fg %1
+
+# Suspender o sleep 300
+
+# Traz o sleep 100 para foreground
+bg %3
+
+# Lista os processos do terminal
+jobs
+
+# Mata todos os processos sleep
+kill %1 %2 %3
