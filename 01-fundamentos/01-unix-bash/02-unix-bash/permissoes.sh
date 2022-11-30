bf30897ace3d49357b1c56fae2ec848422b16224
Marlon Couto <marloncouto.dev@gmail.com>
2022-11-30 15:44:29 -0300

    Adiciona os arquivos permissoes.sh e arquivo_teste.txt
    Signed-off-by: Marlon Couto <marloncouto.dev@gmail.com>

---
+++ 01-fundamentos/01-unix-bash/02-unix-bash/permissoes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Cria o diretório unix_tests_permissions e navega até ela
+mkdir unix_tests_permissions
+cd unix_tests_permissions
+
+# Interage com o arquivo arquivo_teste.txt
+ls -l
+touch arquivo_teste.txt
+chmod 444 arquivo_teste.txt
+ls -ls
